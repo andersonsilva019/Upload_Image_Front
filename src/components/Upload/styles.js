fe7662b5944bce7085115d4fbd67cb9a7b33bd96
Anderson0017 <andeerson0017@gmail.com>
2020-04-27 15:02:04 -0300

    Iniciando o estilo do upload

---
+++ src/components/Upload/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export default DropContainer = styled.div.attrs({
+  className: "dropzone"
+}) `
+  border: 1px dashed #ddd;
+  border-radius: 4px;
+  cursor: pointer;
+
+  transition: height 0.2s ease;
+
+`;
